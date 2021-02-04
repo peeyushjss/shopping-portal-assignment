406677f6c5c56fb7e9a1647368441ae86d50f051
peeyushjss <peeyushjss@gmail.com>
2021-02-04 16:42:29 +0530

    uploaded the zip file.

--- bin/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const server = require('../shopping-server/server'),
-    config = require('../shopping-server/config/config');
-
-server.listen(process.env.port || config.server.port);
-
-console.log('Shopping Portal app started at :', process.env.port || config.server.port); 
